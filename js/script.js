b1a1023aa2345f2005624a8dcf79e5e49486c48a
Carlos Fernando Jaramillo Galindez <109829453+CJaramillo912@users.noreply.github.com>
2026-03-20 13:12:35 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -214,7 +214,7 @@ function validarLetra(letra){
 
     //Si ya se acertaron todas las letras
     if(banderaGanar == palabra.length){
-        mensajeJuegoterminado("Juego completado :D", "win");
+        mensajeJuegoterminado("¡Lo lograste! En un lapso de 24 horas te llegará un regalo a tu cuenta de Steam", "win");
     }
 }
 
